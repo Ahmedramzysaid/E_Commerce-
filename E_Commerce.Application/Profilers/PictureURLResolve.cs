ab6766d22d9724dca83e3682a939073daf1bec6f
Ahmed Ramzy <ramzyis258@gmail.com>
2026-06-29 15:55:34 +0300

    Specification Patterns & URL Resolver

---
+++ E_Commerce.Application/Profilers/PictureURLResolve.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using AutoMapper.Execution;
+using E_Commerce.Application.DTOs.Products;
+using E_Commerce.Domain.Data.Products;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Profilers
+{
+    public class PictureURLResolve : IValueResolver<Product, ProductDTO, string>
+    {
+       public  UrlSetteings urlSetteings { get; set; }
+        public PictureURLResolve(IOptions<UrlSetteings> opt)
+        {
+            urlSetteings = opt.Value;
+        }
+
+
+        public string Resolve(Product source, ProductDTO destination, string destMember, ResolutionContext context)
+        {
+            var baseurl  = urlSetteings.BaseURL.TrimEnd('/');
+            var pathurl = source.PictureUrl.TrimStart('/');
+
+            return $"{baseurl}/Files/{pathurl}";
+
+
+
+
+
+        }
+    }
+    public class UrlSetteings
+    {
+       public string BaseURL { get; set; }
+    }
+
+}
---
+++ E_Commerce.Domain/Contracts/ISpecification.cs
@@ -0,0 +1,20 @@
+﻿using E_Commerce.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contracts
+{
+    public interface ISpecification<TEntity , Tkey> where  TEntity : BaseEntity<Tkey>
+    {
+        // why  not make set becuase i want control  about who  can adding  by  only  calling 
+        //  i  mean  not allow any  componets  not calling this can  see  this prop .  
+        public ICollection<Expression<Func<TEntity ,  object>>> IncludeExpressions { get; }
+        public Expression<Func<TEntity  ,  bool>> Condition { get;  }
+         
+
+    }
+}
